307f376ba80d8b373086c8b427c05fa8f2ad72a5
Grace229 <itiunggrace@gmail.com>
2022-11-02 06:55:14 +0100

    make it responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ProfileImage from "../assets/img/profile__img.svg"
 import ShareImage from "../assets/img/desktop-share.svg"
+import MobileImage from "../assets/img/mobile-share.svg"
 
 const Header = () => {
   return (
     <div className='header'>
         <img src={ShareImage} alt="share" id='share' />
+        <img src={MobileImage} alt="share" id='share1' />
         <div className='header-items'>
         <img src={ProfileImage} alt="profileImg" id='profile__img' />
      <p id='twitter'>rissamo2</p>
+     <p id='slack'>itiunggrace</p>
         </div>
     </div>
   )
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -6,16 +6,18 @@ import GithubIcon from '../assets/img/Social icon.svg'
 const Link = () => {
   return (
     <div className='link-container'>
-        <a href="https://training.zuri.team/" id='btn__zuri'>Zuri Team</a>
-        <a href="https://books.zuri.team/" id='books'>Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=itiunggrace" id='book__python'>Python Books</a>
-        <a href="https://background.zuri.team/" id='pitch'>
+        <a href="https://twitter.com/rissamo2"  target="_blank" id='btn__zuri' className='link' >Twitter Link</a>
+        <a href="https://training.zuri.team/" target="_blank" id='btn__zuri' className='link'>Zuri Team</a>
+        <a href="https://books.zuri.team/" target="_blank" id='books' className='link'>Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=itiunggrace" target="_blank" id='book__python' className='link'>Python Books</a>
+        <a href="https://background.zuri.team/" target="_blank" id='pitch' className='link'>
         Background Check for Coders
         </a>
-        <a href="https://books.zuri.team/design-rules" type='button' id='book__design'>Design Books</a>
+        <a href="https://books.zuri.team/design-rules" type='button' target="_blank" id='book__design' className='link'>Design Books</a>
         <div className='social'>
-            <img src={SlackIcon} alt="" />
-            <img src={GithubIcon} alt="" />
+            <a href="https//slack.com/itiunggrace" target="_blank"><img src={SlackIcon} className="slack" alt="slack" /></a>
+            
+           <a href="https://github.com/grace229/hng" target="_blank"> <img src={GithubIcon} alt="github" /></a>
         </div>
 
     </div>
